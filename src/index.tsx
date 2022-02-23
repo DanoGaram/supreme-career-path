19f97e15c7fcbcb1b764212bdf3b1eef2fcd4269
Daniel <01xdanielx10@gmail.com>
2022-02-23 16:04:38 -0600

    inital commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+console.log("Hello Worlds!");
+
+ReactDOM.render(
+  <React.StrictMode>Holi</React.StrictMode>,
+  document.getElementById("root")
+);
